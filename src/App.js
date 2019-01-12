fd585ec052fbe26d64a87521509043f81d92f2aa
steve hanlon <rshanlon@gmail.com>
2019-01-11 21:12:45 -0800

    Refactor App component so State gave each reel its own object; each reel now runs with its own number of spins and with its own time parameter in setInterval

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+/*NOTES:  To vary results of other two slots, vary speed/time in setInterval using Math.random*/
+
 const slotChoices = [
   "cherry",
   "banana",
@@ -18,69 +20,114 @@ class App extends React.Component {
     super(props);
     this.state = {
       content: slotChoices,
-      choice:  "cherry",
-      index: 0,
-      counter: 0,
-      numberOfSpins: 12, // spinReelAmount declared
+      reel1: {pick: "apple", index: 0, counter: 0, numberOfSpins: 12},
+      reel2: {pick: "apple", index: 0, counter: 0, numberOfSpins: 16},
+      reel3: {pick: "apple", index: 0, counter: 0, numberOfSpins: 20}
     }
-    this.spin = null;
+    this.reel1spin = null;
+    this.reel2spin = null;
+    this.reel3spin = null;
   }
 
 
-  spinOnce = (arr) => {
+  spinReel1once = (arr) => {
     arr = this.state.content;
-    console.log(this.state.index,
-                arr.length,
-                this.state.counter,
-                this.state.numberOfSpins);
     this.setState( prevState => {
       return {
-        choice: arr[this.state.index],
-        counter: prevState.counter + 1,
-        index: prevState.index < arr.length-1 ?
-        prevState.index + 1
-        :
-        this.resetIndexToZero(),
-      }
+        reel1:
+          { ...prevState.reel1,
+            pick: arr[this.state.reel1.index],
+            counter: prevState.reel1.counter + 1,
+            index: prevState.reel1.index < arr.length-1 ?
+            prevState.reel1.index + 1
+            :
+            0 }
+      } //end return
     });
 
-    if (this.state.counter >= this.state.numberOfSpins) {
-      clearInterval(this.spin);
-      this.resetReel();
+    if (this.state.reel1.counter >= this.state.reel1.numberOfSpins) {
+      clearInterval(this.reel1spin);
     }
   }
 
-  resetIndexToZero = () => {
-     this.setState({ index: 0 });
+  spinReel2once = (arr) => {
+    arr = this.state.content;
+    this.setState( prevState => {
+      return {
+        reel2:
+          { ...prevState.reel2,
+            pick: arr[this.state.reel2.index],
+            counter: prevState.reel2.counter + 1,
+            index: prevState.reel2.index < arr.length-1 ?
+            prevState.reel2.index + 1
+            :
+            0 }
+      } //end return
+    });
+
+    if (this.state.reel2.counter >= this.state.reel2.numberOfSpins) {
+      clearInterval(this.reel2spin);
+    }
+  }
+
+  spinReel3once = (arr) => {
+    arr = this.state.content;
+    this.setState( prevState => {
+      return {
+        reel3:
+          { ...prevState.reel3,
+            pick: arr[this.state.reel3.index],
+            counter: prevState.reel3.counter + 1,
+            index: prevState.reel3.index < arr.length-1 ?
+            prevState.reel3.index + 1
+            :
+            0 }
+      } //end return
+    });
+
+    if (this.state.reel3.counter >= this.state.reel3.numberOfSpins) {
+      clearInterval(this.reel3spin);
+      this.resetCount();
+    }
+  }
+
+  resetCount = () => {
+    this.setState( prevState => {
+      return {
+        reel1:{ ...prevState.reel1, counter: 0 },
+        reel2:{ ...prevState.reel2, counter: 0 },
+        reel3:{ ...prevState.reel3, counter: 0 }
+      } //end return
+    });
   }
 
   spinCycle = () => {
-    this.spin = setInterval(this.spinOnce, 100, slotChoices)
+    this.reel1spin = setInterval(this.spinReel1once, 100, this.state.content);
+    this.reel2spin = setInterval(this.spinReel2once, 110, this.state.content);
+    this.reel3spin = setInterval(this.spinReel3once, 130, this.state.content);
   }
 
   spinAmount = (thisAmount) => {
     this.setState({
-      numberOfSpins: thisAmount,
+      reel1: {...this.state.reel1, numberOfSpins: thisAmount},
+      reel2: {...this.state.reel2, numberOfSpins: thisAmount+4},
+      reel3: {...this.state.reel3, numberOfSpins: thisAmount+3},
     }, this.spinCycle());
   }
 
-  resetReel = () => {
-    this.setState({
-      choices: slotChoices[0],
-      counter: 0,
-    });
-  }
-
   render() {
+      console.log(this.state.reel1,
+                this.state.reel2,
+                this.state.reel3);
     return (
       <main>
         <header>
           <h1 id="title">Lost Wages</h1>
         </header>
         <section className="spindle">
-          <Slot result={this.state.choice} />
-          <Slot result={this.state.choice} />
-          <Slot result={this.state.choice} />
+          <Slot result={this.state.reel1.pick} />
+          <Slot result={this.state.reel2.pick} />
+          <Slot result={this.state.reel3.pick} />
         </section>
         <SpinButton spinAmount={this.spinAmount} />
       </main>
